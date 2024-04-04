37b75cb0bca76bcc82d76a5cb9005338a494de8b
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-04 02:38:03 -0400

    added exit variable that is updated hopefully works.

--- EveOnlineAbyssTracker/program.py
+++ EveOnlineAbyssTracker/program.py
@@ -2,10 +2,11 @@ from colorama import *
 
 starting_inv = None  # Global variable to store starting_inv
 total_profit = 0
+exit_program = False  # Flag to indicate when to exit the main menu loop
 
 def menu():
-    global total_profit
-    while True:
+    global total_profit, exit_program
+    while not exit_program:
         menu_text = f"""{Fore.GREEN}Welcome to my EVE ONLINE Python projects.
 1. Abyss Money Tracker
 2. Exit
@@ -25,7 +26,7 @@ def menu():
             print(f"\n{Fore.YELLOW}Invalid choice. Please enter 1 or 2.")
 
 def abyssMain():
-    global starting_inv  # Use the global starting_inv variable
+    global starting_inv, exit_program  # Use the global variables
     if starting_inv is None:
         starting_inv = startingInven()  # Ask for starting_inv only if it's None
 
@@ -36,8 +37,9 @@ def abyssMain():
 
         user_input = input(f"{Fore.GREEN}Track another Abyssal run? (Y/N): {Fore.RED}")
         if user_input.upper() == "N":
-            print(f"\n{Fore.GREEN}Total Profits: {total_profit}m\n")
-            break  # Exit the loop and return to main menu
+            print(f"\n{Fore.GREEN}Total Profits: {total_profit}m")
+            exit_program = True
+            break  # Exit the loop to return to main menu
         elif user_input.upper() != "Y":
             print(f"{Fore.YELLOW}Invalid input. Please enter Y or N.")
 
