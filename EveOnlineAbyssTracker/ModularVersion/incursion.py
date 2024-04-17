557669704aff0a79ff0dd2a508f418c08ed0c751
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-16 23:25:36 -0400

    finished without user validation and user input.

--- EveOnlineAbyssTracker/ModularVersion/incursion.py
+++ EveOnlineAbyssTracker/ModularVersion/incursion.py
@@ -10,32 +10,41 @@ def incursion():
     print(title)
     IncursionProfit = int(input(f"{Fore.GREEN}Profit per incursion: {Fore.RED}"))
     while True:
-        if not runningIncursion(IncursionProfit):
+        if not Menu(IncursionProfit):
             break
 
-def runningIncursion(profitPerRun):
+def Menu(profitPerRun):
     global incProfit
     global title
     global incursiontime
     while True:
         running = input(f"{Fore.GREEN}Still running (Y/N): {Fore.RED}")
         if running.upper() == "Y":
-            os.system("CLS")
-            incProfit += profitPerRun
-            print(title)
-            finishTime = int(input(f"{Fore.GREEN}Time to Finish: {Fore.RED}"))
-            incursiontime += finishTime
-            print(f"{Fore.GREEN}Profit Recorded")
-            time.sleep(2)
-            os.system("CLS")
-            print(f"{Fore.RED}{title}")
-            break  # Break out of the inner loop
+            runningIncursion(profitPerRun)
         elif running.upper() == "N":
-            os.system("CLS")
-            print(f"{Fore.GREEN}Total Profit: {Fore.RED}{incProfit} Million")
-            print(f"{Fore.GREEN}Total Time: {Fore.RED}{incursiontime} Mins")
-            print(f"{Fore.GREEN}Exiting incursion.")
-            return False  # Return False to indicate the function should exit
+            notRunning()
+            return False # Return False to indicate the function should exit
         else:
             print("Invalid input. Press Y if running again, or N if done.")
     return True  # Return True if the user is still running
+
+def runningIncursion(profitPerRun):
+    global incProfit
+    global title
+    global incursiontime
+    os.system("CLS")
+    incProfit += profitPerRun
+    print(title)
+    finishTime = int(input(f"{Fore.GREEN}Time to Finish: {Fore.RED}"))
+    incursiontime += finishTime
+    print(f"{Fore.GREEN}Profit Recorded")
+    time.sleep(2)
+    os.system("CLS")
+    print(f"{Fore.RED}{title}")
+    return  # Break out of the inner loop
+
+def notRunning():
+    os.system("CLS")
+    print(f"{Fore.GREEN}Total Profit: {Fore.RED}{incProfit} Million")
+    print(f"{Fore.GREEN}Total Time: {Fore.RED}{incursiontime} Mins")
+    print(f"{Fore.GREEN}Exiting incursion.")
