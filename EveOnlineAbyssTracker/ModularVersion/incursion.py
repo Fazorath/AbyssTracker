d48d7869162b42b13bd8778e45e6c5b370fab294
Yoenis Hernandez <Hernandezyoenis@gmail.com>
2024-04-16 23:09:10 -0400

    The branch is done i thinkl. very owee

--- EveOnlineAbyssTracker/ModularVersion/incursion.py
+++ EveOnlineAbyssTracker/ModularVersion/incursion.py
@@ -1,31 +1,40 @@
 from colorama import *
+import time
 import os
 from globals import *
 
 profit = 0
 
 def incursion():
-    IncursionProfit = int(input("Profit per incursion: "))
+    global title
+    print(title)
+    IncursionProfit = int(input(f"{Fore.GREEN}Profit per incursion: {Fore.RED}"))
     while True:
         if not runningIncursion(IncursionProfit):
             break
 
 def runningIncursion(profitPerRun):
     global incProfit
+    global title
     global incursiontime
     while True:
-        running = input("Still running (Y/N): ")
+        running = input(f"{Fore.GREEN}Still running (Y/N): {Fore.RED}")
         if running.upper() == "Y":
+            os.system("CLS")
             incProfit += profitPerRun
-            print(incProfit)
-            time = int(input("Time to Finish: "))
-            incursiontime += time
-            print("Profit Recorded")
+            print(title)
+            finishTime = int(input(f"{Fore.GREEN}Time to Finish: {Fore.RED}"))
+            incursiontime += finishTime
+            print(f"{Fore.GREEN}Profit Recorded")
+            time.sleep(2)
+            os.system("CLS")
+            print(f"{Fore.RED}{title}")
             break  # Break out of the inner loop
         elif running.upper() == "N":
-            print(f"Total Profit: {incProfit}Million")
-            print(f"Total Time: {incursiontime}Mins")
-            print("Exiting incursion.")
+            os.system("CLS")
+            print(f"{Fore.GREEN}Total Profit: {Fore.RED}{incProfit} Million")
+            print(f"{Fore.GREEN}Total Time: {Fore.RED}{incursiontime} Mins")
+            print(f"{Fore.GREEN}Exiting incursion.")
             return False  # Return False to indicate the function should exit
         else:
             print("Invalid input. Press Y if running again, or N if done.")
